b8b0493dbf0d0ba25a6a152fe4fe139da6d6c161
konrad <konrad.gornicki@yahoo.com>
2020-07-10 08:06:47 +0100

    changed bag processing file to reflect v4.py file

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Set of various bash scripts
 
 # Add following to bashrc, so scrpits can be executed:
 export PATH="$HOME/path/to/scripts:$PATH
+
+# The scripts need to be added to /usr/local/bin PATH in order
+# to execute them globally
--- mainPC/bag_processing.sh
+++ mainPC/bag_processing.sh
@@ -7,6 +7,7 @@ do
   python bag_to_csv.py $file
 done
 
-#convert to .csv files that matlab can work with:
-python process_csv_file_v3.py
+# convert to .csv files that matlab can work with:
+# python process_csv_file_sideways_v3.py
+process_csv_file_error_analysis_v4.py
 
